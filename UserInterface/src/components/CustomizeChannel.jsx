104da21c47c1a77ab8cbe9637b8977a5d011d626
varshit gupta <varshitguta463@gmail.com>
2024-05-18 20:24:57 +0530

    update

--- UserInterface/src/components/CustomizeChannel.jsx
+++ UserInterface/src/components/CustomizeChannel.jsx
@@ -24,7 +24,7 @@ function CustomizeChannel() {
 
     <label for="email" class="block mb-1 text-sm font-medium text-gray-900 ">Password</label>
     <p id="helper-text-explanation" class="mb-3 text-sm text-gray-500  ">Choose a channel name that represents you and your content. Changes made to your name and picture are visible only on YouTube and not other Google services. You can change your name twice in 14 days. </p>
-    <input type="email" id="email" aria-describedby="helper-text-explanation" className="mb-3 bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="@15#gilA#"/>
+    <input type="email" id="email" aria-describedby="helper-text-explanation" className="mb-4 bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="@15#gilA#"/>
 
      
     </form>
