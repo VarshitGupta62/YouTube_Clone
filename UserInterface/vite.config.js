5aebfc1a7f488903e3c180a73960e0b73edd0533
varshit gupta <varshitguta463@gmail.com>
2024-05-21 11:42:36 +0530

    youtube

--- UserInterface/vite.config.js
+++ UserInterface/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api/v1/': {
+        target: 'http://localhost:8000', // Update this to your backend server's URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1') // Corrected rewrite function
+      }
+    }
+  }
+});
