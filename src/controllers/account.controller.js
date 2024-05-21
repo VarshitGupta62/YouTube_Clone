708bf6b3cac36053822917f31f18947d584ceb8a
varshit gupta <varshitguta463@gmail.com>
2024-05-21 08:55:01 +0530

    youtube

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,11 +1,24 @@
 import { newUser } from "../models/account.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser =  asyncHandler( async (req , res) =>{
      
 
     const { name , email, password } = req.body
     console.log(name);
+
+    const checkUser = await newUser.findOne(
+        {
+            $or: [{name} , {email}]
+        }
+    )
+
+    if(checkUser){
+        // return res.status(400).json({message: "User already exists" , data: null})
+        throw new ApiError(409, "User with email or username already exists")
+    }
      
 
     const user = await newUser.create({
@@ -13,9 +26,9 @@ const registerUser =  asyncHandler( async (req , res) =>{
          email , 
          password
     })
-    console.log(user);
+    // console.log(user);
 
-    return res.status(201).json({messege: "user add" , user})
+    return res.status(201).json(new ApiResponse(200 , user ,"User created successfully"))
 
 }) 
 
