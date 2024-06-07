a230cba747c6184344be788feed2db84334de37c
varshit gupta <varshitguta463@gmail.com>
2024-06-07 11:42:57 +0530

    Updated

--- UserInterface/src/components/Signup.jsx
+++ UserInterface/src/components/Signup.jsx
@@ -63,7 +63,7 @@ function Signup() {
         :
         <div className="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0 bg-slate-100 ">
             <a href="/" className="flex items-center justify-center mb-8 text-2xl font-semibold lg:mb-10 ">
-                <img src={logo} className="mr-4 h-11" alt="FlowBite Logo" />
+                <img src={logo} className="mr-4 h-11" alt="Logo" />
             </a>
             <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow ">
                 <h2 className="text-2xl font-bold text-gray-900 ">
